4139a842ce07740f3e8f6fc16540e5f2b2fadc34
PavieSylvain <pavie.sylvain@sfr.fr>
2022-01-19 23:17:00 +0100

    maj

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -29,7 +29,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="array")
      */
     private $roles = [];
 
